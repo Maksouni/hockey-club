9a6e644d64410aeb1664f9d8a5a402df24dd60df
Maksouni <max1205325@gmail.com>
2025-03-12 22:39:14 +0700

    router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import { Box, Typography } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <Box>
-      <Typography>tewxt</Typography>
-    </Box>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
